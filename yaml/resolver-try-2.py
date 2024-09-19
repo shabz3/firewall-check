a9b34cddf3b180213ba9ac9b1df1ca3153cf4445
shaiban.luqman <shaiban.luqman@garrison.com>
2024-09-19 18:45:02 +0100

    remove

--- yaml/resolver-try-2.py
+++ yaml/resolver-try-2.py
@@ -1,14 +1,4 @@
 
-__all__ = ['BaseResolver', 'Resolver']
-
-from .error import *
-from .nodes import *
-
-import re
-
-class ResolverError(YAMLError):
-    pass
-
 class BaseResolver:
 
     DEFAULT_SCALAR_TAG = 'tag:yaml.org,2002:str'
@@ -18,11 +8,3 @@ class BaseResolver:
     yaml_implicit_resolvers = {}
     yaml_path_resolvers = {}
 
-    def __init__(self):
-        self.resolver_exact_paths = []
-        self.resolver_prefix_paths = []
-
-    @classmethod
-    def add_implicit_resolver(cls, tag, regexp, first):
-        if not 'yaml_implicit_resolvers' in cls.__dict__:
-            implicit_resolvers = {}
