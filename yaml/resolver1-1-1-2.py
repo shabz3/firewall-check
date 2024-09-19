284e0514ec546a56874b28a51f7e12081cd0a8a4
shaiban.luqman <shaiban.luqman@garrison.com>
2024-09-19 18:18:52 +0100

    1-1-1-1 and 1-1-1-2

--- yaml/resolver1-1-1-2.py
+++ yaml/resolver1-1-1-2.py
@@ -1,17 +1,3 @@
-
-__all__ = ['BaseResolver', 'Resolver']
-
-from .error import *
-from .nodes import *
-
-import re
-
-class ResolverError(YAMLError):
-    pass
-
-class BaseResolver:
-
-    DEFAULT_SCALAR_TAG = 'tag:yaml.org,2002:str'
     DEFAULT_SEQUENCE_TAG = 'tag:yaml.org,2002:seq'
     DEFAULT_MAPPING_TAG = 'tag:yaml.org,2002:map'
 
