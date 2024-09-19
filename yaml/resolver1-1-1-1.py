fa026191a88c6d36bab1fdbb9eb7bd7aadec331d
shaiban.luqman <shaiban.luqman@garrison.com>
2024-09-19 18:32:02 +0100

    put back

--- yaml/resolver1-1-1-1.py
+++ yaml/resolver1-1-1-1.py
@@ -21,3 +21,8 @@ class BaseResolver:
     def __init__(self):
         self.resolver_exact_paths = []
         self.resolver_prefix_paths = []
+
+    @classmethod
+    def add_implicit_resolver(cls, tag, regexp, first):
+        if not 'yaml_implicit_resolvers' in cls.__dict__:
+            implicit_resolvers = {}
