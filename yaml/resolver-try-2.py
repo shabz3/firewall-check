7d76b596516b5347eafde4a32dfffdc0a6ac1c4c
shaiban.luqman <shaiban.luqman@garrison.com>
2024-09-20 07:04:32 +0100

    remove classmethod

--- yaml/resolver-try-2.py
+++ yaml/resolver-try-2.py
@@ -22,7 +22,3 @@ class BaseResolver:
         self.resolver_exact_paths = []
         self.resolver_prefix_paths = []
 
-    @classmethod
-    def add_implicit_resolver(cls, tag, regexp, first):
-        if not 'yaml_implicit_resolvers' in cls.__dict__:
-            implicit_resolvers = {}
