782839ac95dce01a20ac0e4dbf9cce4b6a5e50f1
shaiban.luqman <shaiban.luqman@garrison.com>
2024-09-26 16:06:19 +0100

    __main___copy.py

--- pipenv/patched/pip/__main___copy.py
+++ pipenv/patched/pip/__main___copy.py
@@ -1,12 +1,3 @@
-import os
-import sys
-
-# Remove '' and current working directory from the first entry
-# of sys.path, if present to avoid using current directory
-# in pip commands check, freeze, install, list and show,
-# when invoked as python -m pip <command>
-if sys.path[0] in ("", os.getcwd()):
-    sys.path.pop(0)
 
 # If we are running from a wheel, add the wheel to sys.path
 # This allows the usage python pip-*.whl/pip install pip-*.whl
@@ -24,3 +15,8 @@ if __name__ == "__main__":
     spec = importlib.util.spec_from_file_location(
         "pipenv", location=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "__init__.py"))
     pipenv = importlib.util.module_from_spec(spec)
+    sys.modules["pipenv"] = pipenv
+    spec.loader.exec_module(pipenv)
+    from pipenv.patched.pip._internal.cli.main import main as _main
+
+    sys.exit(_main())
