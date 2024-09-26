5516fc69fb0e3f9b7e2daf73779bf17814e234d4
shaiban.luqman <shaiban.luqman@garrison.com>
2024-09-26 16:00:29 +0100

    undo

--- pipenv/patched/pip/main1.py
+++ pipenv/patched/pip/main1.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+# Remove '' and current working directory from the first entry
+# of sys.path, if present to avoid using current directory
+# in pip commands check, freeze, install, list and show,
+# when invoked as python -m pip <command>
+if sys.path[0] in ("", os.getcwd()):
+    sys.path.pop(0)
 
 # If we are running from a wheel, add the wheel to sys.path
 # This allows the usage python pip-*.whl/pip install pip-*.whl
