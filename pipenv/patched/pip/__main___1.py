a385a846bdfdc6a223c2b79595aeefd47505fbf8
shaiban.luqman <shaiban.luqman@garrison.com>
2024-09-26 16:12:02 +0100

    1 and 2

--- pipenv/patched/pip/__main___1.py
+++ pipenv/patched/pip/__main___1.py
@@ -17,15 +17,3 @@ if __package__ == "":
     # Add that to sys.path so we can import pip
     path = os.path.dirname(os.path.dirname(__file__))
     sys.path.insert(0, path)
-
-if __name__ == "__main__":
-    import importlib.util
-    import sys
-    spec = importlib.util.spec_from_file_location(
-        "pipenv", location=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "__init__.py"))
-    pipenv = importlib.util.module_from_spec(spec)
-    sys.modules["pipenv"] = pipenv
-    spec.loader.exec_module(pipenv)
-    from pipenv.patched.pip._internal.cli.main import main as _main
-
-    sys.exit(_main())
