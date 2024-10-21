9e8b9d48a0b1f5abc3389fddb1a1bc3a7e6b9f4c
shaiban.luqman <shaiban.luqman@garrison.com>
2024-10-21 14:49:06 +0100

    split file

--- pip/_vendor/pygments/util2.py
+++ pip/_vendor/pygments/util2.py
@@ -1,40 +1,25 @@
-def get_choice_opt(options, optname, allowed, default=None, normcase=False):
-    """
-    If the key `optname` from the dictionary is not in the sequence
-    `allowed`, raise an error, otherwise return it.
-    """
-    string = options.get(optname, default)
-    if normcase:
-        string = string.lower()
-    if string not in allowed:
-        raise OptionError('Value for option {} must be one of {}'.format(optname, ', '.join(map(str, allowed))))
-    return string
+split_path_re = re.compile(r'[/\\ ]')
+doctype_lookup_re = re.compile(r'''
+    <!DOCTYPE\s+(
+     [a-zA-Z_][a-zA-Z0-9]*
+     (?: \s+      # optional in HTML5
+     [a-zA-Z_][a-zA-Z0-9]*\s+
+     "[^"]*")?
+     )
+     [^>]*>
+''', re.DOTALL | re.MULTILINE | re.VERBOSE)
+tag_re = re.compile(r'<(.+?)(\s.*?)?>.*?</.+?>',
+                    re.IGNORECASE | re.DOTALL | re.MULTILINE)
+xml_decl_re = re.compile(r'\s*<\?xml[^>]*\?>', re.I)
 
 
-def get_bool_opt(options, optname, default=None):
-    """
-    Intuitively, this is `options.get(optname, default)`, but restricted to
-    Boolean value. The Booleans can be represented as string, in order to accept
-    Boolean value from the command line arguments. If the key `optname` is
-    present in the dictionary `options` and is not associated with a Boolean,
-    raise an `OptionError`. If it is absent, `default` is returned instead.
+class ClassNotFound(ValueError):
+    """Raised if one of the lookup functions didn't find a matching class."""
 
-    The valid string values for ``True`` are ``1``, ``yes``, ``true`` and
-    ``on``, the ones for ``False`` are ``0``, ``no``, ``false`` and ``off``
-    (matched case-insensitively).
+
+class OptionError(Exception):
     """
-    string = options.get(optname, default)
-    if isinstance(string, bool):
-        return string
-    elif isinstance(string, int):
-        return bool(string)
-    elif not isinstance(string, str):
-        raise OptionError(f'Invalid type {string!r} for option {optname}; use '
-                          '1/0, yes/no, true/false, on/off')
-    elif string.lower() in ('1', 'yes', 'true', 'on'):
-        return True
-    elif string.lower() in ('0', 'no', 'false', 'off'):
-        return False
-    else:
-        raise OptionError(f'Invalid value {string!r} for option {optname}; use '
-                          '1/0, yes/no, true/false, on/off')
+    This exception will be raised by all option processing functions if
+    the type or value of the argument is not correct.
+    """
+
