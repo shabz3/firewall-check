3844ba13d7b0f29c36948c05f31e4aaf81d5cd20
shaiban.luqman <shaiban.luqman@garrison.com>
2024-10-21 14:40:57 +0100

    split file

--- pip/_vendor/pygments/util2.py
+++ pip/_vendor/pygments/util2.py
@@ -1,83 +1,40 @@
-
-
-def get_int_opt(options, optname, default=None):
-    """As :func:`get_bool_opt`, but interpret the value as an integer."""
+def get_choice_opt(options, optname, allowed, default=None, normcase=False):
+    """
+    If the key `optname` from the dictionary is not in the sequence
+    `allowed`, raise an error, otherwise return it.
+    """
     string = options.get(optname, default)
-    try:
-        return int(string)
-    except TypeError:
-        raise OptionError(f'Invalid type {string!r} for option {optname}; you '
-                          'must give an integer value')
-    except ValueError:
-        raise OptionError(f'Invalid value {string!r} for option {optname}; you '
-                          'must give an integer value')
+    if normcase:
+        string = string.lower()
+    if string not in allowed:
+        raise OptionError('Value for option {} must be one of {}'.format(optname, ', '.join(map(str, allowed))))
+    return string
 
-def get_list_opt(options, optname, default=None):
+
+def get_bool_opt(options, optname, default=None):
     """
-    If the key `optname` from the dictionary `options` is a string,
-    split it at whitespace and return it. If it is already a list
-    or a tuple, it is returned as a list.
+    Intuitively, this is `options.get(optname, default)`, but restricted to
+    Boolean value. The Booleans can be represented as string, in order to accept
+    Boolean value from the command line arguments. If the key `optname` is
+    present in the dictionary `options` and is not associated with a Boolean,
+    raise an `OptionError`. If it is absent, `default` is returned instead.
+
+    The valid string values for ``True`` are ``1``, ``yes``, ``true`` and
+    ``on``, the ones for ``False`` are ``0``, ``no``, ``false`` and ``off``
+    (matched case-insensitively).
     """
-    val = options.get(optname, default)
-    if isinstance(val, str):
-        return val.split()
-    elif isinstance(val, (list, tuple)):
-        return list(val)
+    string = options.get(optname, default)
+    if isinstance(string, bool):
+        return string
+    elif isinstance(string, int):
+        return bool(string)
+    elif not isinstance(string, str):
+        raise OptionError(f'Invalid type {string!r} for option {optname}; use '
+                          '1/0, yes/no, true/false, on/off')
+    elif string.lower() in ('1', 'yes', 'true', 'on'):
+        return True
+    elif string.lower() in ('0', 'no', 'false', 'off'):
+        return False
     else:
-        raise OptionError(f'Invalid type {val!r} for option {optname}; you '
-                          'must give a list value')
-
-
-def docstring_headline(obj):
-    if not obj.__doc__:
-        return ''
-    res = []
-    for line in obj.__doc__.strip().splitlines():
-        if line.strip():
-            res.append(" " + line.strip())
-        else:
-            break
-    return ''.join(res).lstrip()
-
-
-def make_analysator(f):
-    """Return a static text analyser function that returns float values."""
-    def text_analyse(text):
-        try:
-            rv = f(text)
-        except Exception:
-            return 0.0
-        if not rv:
-            return 0.0
-        try:
-            return min(1.0, max(0.0, float(rv)))
-        except (ValueError, TypeError):
-            return 0.0
-    text_analyse.__doc__ = f.__doc__
-    return staticmethod(text_analyse)
-
-
-def shebang_matches(text, regex):
-    r"""Check if the given regular expression matches the last part of the
-    shebang if one exists.
-
-        >>> from pygments.util import shebang_matches
-        >>> shebang_matches('#!/usr/bin/env python', r'python(2\.\d)?')
-        True
-        >>> shebang_matches('#!/usr/bin/python2.4', r'python(2\.\d)?')
-        True
-        >>> shebang_matches('#!/usr/bin/python-ruby', r'python(2\.\d)?')
-        False
-        >>> shebang_matches('#!/usr/bin/python/ruby', r'python(2\.\d)?')
-        False
-        >>> shebang_matches('#!/usr/bin/startsomethingwith python',
-        ...                 r'python(2\.\d)?')
-        True
-
-    It also checks for common windows executable file extensions::
-
-        >>> shebang_matches('#!C:\\Python2.4\\Python.exe', r'python(2\.\d)?')
-        True
-
-    Parameters (``'-f'`` or ``'--foo'`` are ignored so ``'perl'`` does
-    the same as ``'perl -e'``)
+        raise OptionError(f'Invalid value {string!r} for option {optname}; use '
+                          '1/0, yes/no, true/false, on/off')
