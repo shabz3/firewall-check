b4560fb3c8de6761255dfa6f155aa34809a327c2
shaiban.luqman <shaiban.luqman@garrison.com>
2024-11-07 13:51:27 +0000

    split once more

--- pip/_vendor/distlib/markers.py
+++ pip/_vendor/distlib/markers.py
@@ -29,13 +29,3 @@ _VERSION_MARKERS = {'python_version', 'python_full_version'}
 
 def _is_version_marker(s):
     return isinstance(s, string_types) and s in _VERSION_MARKERS
-
-
-def _is_literal(o):
-    if not isinstance(o, string_types) or not o:
-        return False
-    return o[0] in '\'"'
-
-
-def _get_versions(s):
-    return {LV(m.groups()[0]) for m in _VERSION_PATTERN.finditer(s)}
