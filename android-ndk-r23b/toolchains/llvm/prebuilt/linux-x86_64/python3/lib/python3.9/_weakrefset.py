0a873d2979cdcc52c4a97380b47be05154003932
Shaiban Luqman <shaiban.luqman@garrison.com>
2024-11-21 14:15:23 +0000

    split

--- android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/python3/lib/python3.9/_weakrefset.py
+++ android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/python3/lib/python3.9/_weakrefset.py
@@ -31,19 +31,3 @@ class _IterationGuard:
             s.remove(self)
             if not s:
                 w._commit_removals()
-
-
-class WeakSet:
-    def __init__(self, data=None):
-        self.data = set()
-        def _remove(item, selfref=ref(self)):
-            self = selfref()
-            if self is not None:
-                if self._iterating:
-                    self._pending_removals.append(item)
-                else:
-                    self.data.discard(item)
-        self._remove = _remove
-        # A list of keys to be removed
-        self._pending_removals = []
-        self._iterating = set()
--- android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/python3/lib/python3.9/_weakrefset2.py
+++ android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/python3/lib/python3.9/_weakrefset2.py
@@ -1,50 +1,16 @@
-        if data is not None:
-            self.update(data)
 
-    def _commit_removals(self):
-        l = self._pending_removals
-        discard = self.data.discard
-        while l:
-            discard(l.pop())
 
-    def __iter__(self):
-        with _IterationGuard(self):
-            for itemref in self.data:
-                item = itemref()
-                if item is not None:
-                    # Caveat: the iterator will keep a strong reference to
-                    # `item` until it is resumed or closed.
-                    yield item
-
-    def __len__(self):
-        return len(self.data) - len(self._pending_removals)
-
-    def __contains__(self, item):
-        try:
-            wr = ref(item)
-        except TypeError:
-            return False
-        return wr in self.data
-
-    def __reduce__(self):
-        return (self.__class__, (list(self),),
-                getattr(self, '__dict__', None))
-
-    def add(self, item):
-        if self._pending_removals:
-            self._commit_removals()
-        self.data.add(ref(item, self._remove))
-
-    def clear(self):
-        if self._pending_removals:
-            self._commit_removals()
-        self.data.clear()
-
-    def copy(self):
-        return self.__class__(self)
-
-    def pop(self):
-        if self._pending_removals:
-            self._commit_removals()
-        while True:
-            try:
+class WeakSet:
+    def __init__(self, data=None):
+        self.data = set()
+        def _remove(item, selfref=ref(self)):
+            self = selfref()
+            if self is not None:
+                if self._iterating:
+                    self._pending_removals.append(item)
+                else:
+                    self.data.discard(item)
+        self._remove = _remove
+        # A list of keys to be removed
+        self._pending_removals = []
+        self._iterating = set()
