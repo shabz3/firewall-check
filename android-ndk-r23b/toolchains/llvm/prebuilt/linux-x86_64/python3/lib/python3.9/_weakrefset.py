71638cefbab63169f928de67a64aa124adc3f768
Shaiban Luqman <shaiban.luqman@garrison.com>
2024-11-21 14:16:36 +0000

    split

--- android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/python3/lib/python3.9/_weakrefset.py
+++ android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/python3/lib/python3.9/_weakrefset.py
@@ -24,10 +24,3 @@ class _IterationGuard:
             w._iterating.add(self)
         return self
 
-    def __exit__(self, e, t, b):
-        w = self.weakcontainer()
-        if w is not None:
-            s = w._iterating
-            s.remove(self)
-            if not s:
-                w._commit_removals()
--- android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/python3/lib/python3.9/_weakrefset2.py
+++ android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/python3/lib/python3.9/_weakrefset2.py
@@ -1,16 +1,7 @@
-
-
-class WeakSet:
-    def __init__(self, data=None):
-        self.data = set()
-        def _remove(item, selfref=ref(self)):
-            self = selfref()
-            if self is not None:
-                if self._iterating:
-                    self._pending_removals.append(item)
-                else:
-                    self.data.discard(item)
-        self._remove = _remove
-        # A list of keys to be removed
-        self._pending_removals = []
-        self._iterating = set()
+    def __exit__(self, e, t, b):
+        w = self.weakcontainer()
+        if w is not None:
+            s = w._iterating
+            s.remove(self)
+            if not s:
+                w._commit_removals()
